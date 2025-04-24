c6ec57364803a45397424274d44bc3a865d065f4
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-04-24 11:36:15 +0530

    Create database.py

---
+++ backend/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("imt.db")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT UNIQUE,
+            phone TEXT,
+            password TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def add_user(data):
+    conn = sqlite3.connect("imt.db")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO users (name, email, phone, password)
+        VALUES (?, ?, ?, ?)
+    ''', (data['firstName'] + " " + data['lastName'], data['email'], data['phone'], data['password']))
+    conn.commit()
+    conn.close()
