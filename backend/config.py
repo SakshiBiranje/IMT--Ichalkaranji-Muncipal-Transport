77f76ba00862ed17a8890d907fc348890652680e
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-04-24 11:36:12 +0530

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    SECRET_KEY = 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/imt.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Email Configuration
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'sakshibiranje7126@gmail.com'  # Replace
+    MAIL_PASSWORD = 'Sakshi1234'     # Replace
